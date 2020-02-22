a01d99cc220f2be2a60faada6a265e6a19318b56
MaximAndreev <to.altor@gmail.com>
2020-02-22 13:24:57 +0300

    Initial commit. Set up Junit.

---
+++ src/test/java/ru/avtomir/toExcel/JunitWorks.java
@@ -0,0 +1,14 @@
+package ru.avtomir.toExcel;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JunitWorks {
+
+    @Test
+    public void nothing_done() {
+        Assertions.assertTrue(true);
+    }
+}
